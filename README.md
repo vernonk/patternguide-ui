ba71baa8b697b078ee60aa6f54621e086c9c42eb
Vernon Kesner <vernonkesner@gmail.com>
2015-11-09 18:45:32 -0500

    Initial rough build out of some things.

---
+++ README.md
@@ -0,0 +1,17 @@
+PatternGuide UI
+---
+
+Example PatternGuide pattern library project.
+
+## Assumptions
+
+1. File Structure
+
+  ```text
+  | dist
+  |   -- (compiled assets)
+  | src  
+  |   -- layouts/
+  |
+  ```
+2. 
---
+++ patternguide.js
@@ -0,0 +1,24 @@
+// Pattern Guide Configuration
+// CLI Overrides should be available for most of these options outside of probably routePrefix... possibly even that.
+module.exports = {
+  baseUrl: "http://localhost:3000", // base url, default 'localhost'
+
+  // PatternGuide supports proxy debugging giving you a seamless local environment
+  proxyHost: "", // base host to reverse proxy against, default ''
+  proxySecure: true, // route proxy requests through https, default true
+
+  // should routes be sent from somewhere other than patternguide/?
+  // this is useful if your site directories conflict with
+  // patternguide (e.g. layouts, patterns, etc )
+  routePrefix: "", // default 'patternguide' <- allows for reverseproxy to serve homepage
+  routePrefix: "patternguide", // default 'patternguide' <- allows for reverseproxy to serve a homepage
+
+  // what templating engine will be used for partials and layouts?
+  // e.g. lodash, underscore, handlebars, hogan, ejs
+  // all template engines supported by consolidate.js are supported in patternguide
+  // https://github.com/tj/consolidate.js#supported-template-engines
+  templating: "ejs", // default 'ejs'
+  templatingFileExt: "ejs", // default 'ejs'
+  // cache partials or layouts? Not sure why you would but hey...
+  templateCache: false // default false
+};
